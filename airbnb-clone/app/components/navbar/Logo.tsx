d64ed89c67729da3d4668178a87619ad516c146c
piupaulgit <contactpiu619@gmail.com>
2023-09-24 19:08:06 +0530

    categories

--- airbnb-clone/app/components/navbar/Logo.tsx
+++ airbnb-clone/app/components/navbar/Logo.tsx
@@ -3,9 +3,19 @@
 import Image from "@/node_modules/next/image";
 import { logo } from "@/public/images/index";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const Logo: React.FC = () => {
-  return <Image alt="air bnb logo" src={logo} height={100} width={100} />;
+  const router = useRouter();
+  return (
+    <Image
+      onClick={() => router.push("/")}
+      alt="air bnb logo"
+      src={logo}
+      height={100}
+      width={100}
+    />
+  );
 };
 
 export default Logo;
